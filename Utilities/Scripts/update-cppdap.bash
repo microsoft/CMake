91d99ed60dff755e39afbae409983ce0188d9869
Glen Chung <kuchung@microsoft.com>
2022-11-11 17:41:56 -0800

    Prepare to Add cppdap Library
    - Will run update-cppdap.bash to get the source files.
    - Do not disable warning 4267.

---
+++ Utilities/Scripts/update-cppdap.bash
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s dotglob
+
+readonly name="cppdap"
+readonly ownership="cppdap Upstream <kwrobot@kitware.com>"
+readonly subtree="Utilities/cmcppdap"
+readonly repo="https://github.com/google/cppdap.git"
+readonly tag="a4bc9773434f045d28a17c27396f469b63fb4f6a"
+readonly shortlog=false
+readonly paths="
+  cmake
+  CMakeLists.txt
+  LICENSE
+  include
+  src
+"
+
+extract_source () {
+    git_archive
+
+    pushd "${extractdir}/${name}-reduced"
+    echo "* -whitespace" > .gitattributes
+    sed -i 's/"\/wd4267"/# "\/wd4267"/' CMakeLists.txt
+    sed -i 's/matched_idx\ +=\ len;/matched_idx\ +=\ \(uint32_t\)len;/' src/content_stream.cpp
+    # Do not install any files.
+    sed -i '0,/install(/s//if(DEFINED _INSTALL_CPPDAP)\ninstall(/' CMakeLists.txt
+    sed -i '0,/DESTINATION ${CPPDAP_CONFIG_INSTALL_DIR})/s//DESTINATION ${CPPDAP_CONFIG_INSTALL_DIR})\nendif()/' CMakeLists.txt
+    # Install the NOTICE file.
+    echo -e "'cppdap' is a C++11 library implementation of the Debug Adapter Protocol
+version 5b796454c1, Jan 05, 2023\nCopyright Google LLC\n\nThis product includes software developed at Google." > NOTICE
+    echo -e "\ninstall(FILES NOTICE DESTINATION \${CMAKE_DOC_DIR}/cmcppdap)" >> CMakeLists.txt
+    echo "" > LICENSE
+    echo "" > src/nlohmann_json_serializer.h
+    popd
+}
+
+. "${BASH_SOURCE%/*}/update-third-party.bash"
